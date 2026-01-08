f04307c6babfb12d2a4803624a517261a02197b3
Leonardo-L-Cunha <leonavidareal@gmail.com>
2026-01-08 14:49:43 -0300

    settings for vision camera

--- Home/index.tsx
+++ Home/index.tsx
@@ -1,19 +1,38 @@
+import { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Container, Title, Logo, Button, ButtonText } from "./style";
+import { useCameraPermission } from "react-native-vision-camera";
+import { Text } from "react-native";
 
 export default function Home() {
     const navigation = useNavigation<any>();
+    const { hasPermission, requestPermission } = useCameraPermission()
+
+    useEffect(() => {
+        const handleAllow = async () => {
+            if (!hasPermission) {
+                const granted = await requestPermission();
+                console.log(granted)
+            }
+        };
+        handleAllow()
+    }, [])
 
     return (
         <Container>
             <Title>LEONARDO L CUNHA</Title>
             <Logo source={{
-                uri:'https://www.pngall.com/wp-content/uploads/5/Pokemon-Go-Logo-PNG-High-Quality-Image.png'
-            }}/>
+                uri: 'https://www.pngall.com/wp-content/uploads/5/Pokemon-Go-Logo-PNG-High-Quality-Image.png'
+            }} />
+            {hasPermission ? (
+                <Button onPress={() => navigation.navigate('Scanner')}>
+                    <ButtonText>Scannear QRCODE</ButtonText>
+                </Button>
+            ) : (
+                <Text>forneça permisao</Text>
+            )}
 
-            <Button onPress={() => navigation.navigate('Scanner')}>
-                <ButtonText>Scannear QRCODE</ButtonText>
-            </Button>
+
         </Container>
     )
 }
