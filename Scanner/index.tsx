b29fa8cdd89bc74a03a962d929157e62c385c3a0
Leonardo-L-Cunha <leonavidareal@gmail.com>
2026-01-09 10:40:15 -0300

    redirecting data

--- Scanner/index.tsx
+++ Scanner/index.tsx
@@ -1,12 +1,37 @@
 import { Container, LoadingText, QrImage, Text } from "./style";
 import { Camera, useCodeScanner, useCameraDevice } from "react-native-vision-camera";
+import { getPokemon } from "../service";
+import { useNavigation } from "@react-navigation/native";
+import { useRef } from "react";
 
 export default function Scanner() {
     const device = useCameraDevice('back')
+    const navigation = useNavigation<any>()
+
+    const isScanning = useRef(false)
+
     const codeScanner = useCodeScanner({
         codeTypes: ['qr', 'ean-13'],
-        onCodeScanned: (codes) => {
-            console.log("data:", codes[0].value)
+        onCodeScanned: async (codes) => {
+            
+            if (!isScanning.current) {
+                return 
+            }
+            
+            const id = Number(codes[0].value?.replace(/\D/g, ""))
+
+            try {
+                isScanning.current = true
+
+                const pokemon  = await getPokemon(id)
+
+                navigation.navigate("Details", {
+                    pokemon
+                });
+            } catch (error) {
+                isScanning.current = false
+            }
+
         }
     })
 
